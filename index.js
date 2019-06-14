10d667b1a52f420ded1c37c6bed929e4bd205a60
boredomed <mrana.bese15seecs@seecs.edu.pk>
2019-06-14 19:15:34 +0500

    routes complete

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ db.connect(function(err){
     console.log('Connected to database');
 });
 
+//creating user
+app.post('/users', function(req, res){
+  let name = req.body.name;
+ 
+  let quer = "Insert into users (Name) VALUES( '"+ name +"')";
+  db.query(quer,function(err,result){
+    if (err) throw err;
+    else res.send("User created");	
+  });
+
+});
+
 //all users
 app.get('/users', function(req, res){
   db.query("Select * from users", function(err,result){
@@ -35,7 +47,7 @@ app.get('/users', function(req, res){
   });
 });
 
-//accesing a particular user
+//getting a particular user
 app.get('/users/:id', function(req, res){
   let id = req.params.id;
   let quer = "Select * from users where ID =" + id;
@@ -50,6 +62,20 @@ app.get('/users/:id', function(req, res){
 
 });
 
+//Visitors Code
+//getting all visits data
+app.get('/visits', function(req, res){
+  db.query("Select * from visits", function(err,result){
+  
+  if (err) throw err
+  if(result.length > 0){
+	res.send(result);
+  }
+  else console.log("No current visit");
+  });
+});
+
+//posting a visits
 app.post('/users/:uid/visit', function(req, res){
   let uid = req.params.uid;
   let desc = req.body.desc, 
@@ -64,15 +90,145 @@ app.post('/users/:uid/visit', function(req, res){
 
 });
 
-app.get('/visits', function(req, res){
-  db.query("Select * from visits", function(err,result){
+//getting all requests data shughal
+app.get('/requests', function(req, res){
+  db.query("Select * from requests", function(err,result){
   
   if (err) throw err
   if(result.length > 0){
 	res.send(result);
   }
-  else console.log("No current visit");
+  else console.log("No current requests");
   });
 });
+
+//getting requests for a particular visit
+app.get('/users/:id/visit/:vid/requests', function(req, res){
+  let vid = req.params.vid;
+  db.query("Select * from requests where VID = " + vid , function(err,result){
+  
+  if (err) throw err
+  if(result.length > 0){
+	res.send(result);
+  }
+  else console.log("No current requests");
+  });
+});
+
+//updating status of reuest ie accepting and declining ie using put
+app.put('/users/:id/visit/:vid/request/:rid', function(req, res){
+  let rid = req.params.rid;
+  let status = req.body.status;
+
+  let quer = "Update requests set Status = '"+status+"' where ID = " + rid;
+
+  db.query(quer,function(err,result){
+    if (err) throw err;
+  	res.send("Request status set");	
+  });
+});
+
+
+//Requester part
+//posting request
+app.post('/visits/:vid/requests', function(req, res){
+  let vid = req.params.vid;
+  let desc = req.body.desc; 
+  let uid = req.body.uid;
+
+  let quer = "Insert into requests (Description, UID, VID) VALUES( '"+ desc +"', "+uid+", "+vid+")";
+  db.query(quer,function(err,result){
+    if (err) throw err;
+    else res.send("Request created");	
+  });
+
+});
+
+//getting all requests for that user
+app.get('/users/:id/requests', function(req, res){
+  let uid = req.params.id;
+  console.log(uid);
+  db.query("Select * from requests where UID = " + uid , 
+
+function(err,result){
+  
+  if (err) throw err
+  if(result.length > 0){
+	res.send(result);
+  }
+  else console.log("No requests posted");
+  });
+});
+
+//Requester accepting/declining requests that were unavailable
+app.put('/users/:id/requests/:rid', function(req, res){
+  let rid = req.params.rid;
+  let status = req.body.status;
+  db.query("Update requests set Status = '"+status+"' where ID = " + rid, 
+function(err,result){
+  if (err) throw err
+  else res.send("Request status changed from Not available");
+});
+
+});
+
+//Completing a visit and earning points
+app.put('/users/:id/visit/:vid', function(req, res){
+
+  let uid = req.params.id;
+  let vid = req.params.vid;
+  let status = req.body.status;
+
+  //finding if any request is marked 'not available'
+  let quer = "select count(Status) as coun from requests where Status = 'Not Available' and VID = " + vid + " and UID = " + uid;
+  //update visits
+  let quer1 = "update visits set Status = '"+status+"' where ID = " + vid + " and UID = " + uid;
+  //calculating the total completed 
+  let quer2 = "select count(Status) as coun1 from requests where Status = 'Completed' and VID = " + vid + " and UID = " + uid;
+
+  db.query(quer, function(err, result){
+
+  count = result[0]["coun"];
+  if (err) throw err;
+  else{
+	if(count > 0){
+		res.send("Request cannot be completed as some request are marked Not available");
+	}else{ 
+		// req complete
+		// Updating status of visit
+		db.query(quer1, function(err, result1){
+		  if(err) throw err;
+		  else res.send("Visit marked Completed");
+		});
+
+		db.query(quer2, function(err, result2){
+
+			countComp = result2[0]["coun1"];
+			let points;
+
+			if(countComp == 0){
+				points = 0;
+			}
+			else if(countComp > 0){
+				reqPoint = countComp*5;
+				points = 5 + reqPoint;
+			}
+			console.log(points);
+
+			//points
+			let quer3 = "update users set points = " + points + " where ID =" + uid;
+
+			//Updating points of user
+			db.query(quer3, function(err, result){
+			  if(err) throw err;
+			  else console.log("Updated points");
+			});
+				
+		});
+	     }
+      }
+});
+});
+
 
 app.listen(3000);
