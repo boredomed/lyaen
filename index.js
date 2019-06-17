f08164af24b13045f6380e9e6d64dd50155fee96
Afzaal Ahmad <aahmad.bese15seecs@seecs.edu.pk>
2019-06-17 13:00:56 +0500

    Adding views and updated visit status change code!

--- index.js
+++ index.js
@@ -9,29 +9,29 @@ var db = mysql.createConnection({
     database: "lyaen"
 });
 
-db.connect(function(err){
+db.connect(function (err) {
     if (err) throw err;
     console.log("Connected");
 });
 app.use(express.json());
 
-app.get('/',function(req, res){
+app.get('/', function (req, res) {
     res.send("Welcome to Lyaen API.");
 });
 
 app.get('/users', function (req, res) {
     var sql = "select * from users;";
-    db.query(sql, function(err, result){
+    db.query(sql, function (err, result) {
         if (err) throw err;
         res.send(result);
     })
     //res.end();
 });
 
-app.get('/users/:uid', function(req, res){
+app.get('/users/:uid', function (req, res) {
     var uid = req.params.uid;
     var sql = "select * from users where ID = ?;";
-    db.query(sql, [uid], function(err, result){
+    db.query(sql, [uid], function (err, result) {
         if (err) throw err;
         res.send(result);
     });
@@ -39,7 +39,7 @@ app.get('/users/:uid', function(req, res){
 
 app.get('/visits', function (req, res) {
     var sql = "select * from visits where status<>'Completed' ;";
-    db.query(sql, function(err, result){
+    db.query(sql, function (err, result) {
         if (err) throw err;
         res.send(result);
     })
@@ -49,36 +49,36 @@ app.get('/visits', function (req, res) {
 app.get('/visits/:vid', function (req, res) {
     var vid = req.params.vid;
     var sql = "select * from visits WHERE ID = ?;";
-    db.query(sql, [vid] ,function(err, result){
+    db.query(sql, [vid], function (err, result) {
         if (err) throw err;
         res.send(result);
     })
     //res.end();
 });
 
-app.get('/users/:uid/visits', function(req, res){
+app.get('/users/:uid/visits', function (req, res) {
     var uid = req.params.uid;
     var sql = "select * from visits where UID = ?;";
-    db.query(sql, [uid], function(err, result){
+    db.query(sql, [uid], function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
 
-app.get('/users/:uid/visits/:vid', function(req, res){
+app.get('/users/:uid/visits/:vid', function (req, res) {
     var uid = req.params.uid;
     var vid = req.params.vid
     var sql = "select * from visits where UID = ? and ID = ?;";
-    db.query(sql, [uid,vid], function(err, result){
+    db.query(sql, [uid, vid], function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
 
-app.get('/users/:uid/visits/:vid/requests', function(req, res){
+app.get('/users/:uid/visits/:vid/requests', function (req, res) {
     var vid = req.params.vid;
     var sql = "select * from requests where VID = ?;";
-    db.query(sql, [vid], function(err, result){
+    db.query(sql, [vid], function (err, result) {
         if (err) throw err;
         res.send(result);
     });
@@ -95,110 +95,114 @@ app.get('/users/:uid/visits/:vid/requests', function(req, res){
 //     //res.end();
 // });
 
-app.get('/users/:uid/requests', function(req, res){
+app.get('/users/:uid/requests', function (req, res) {
     var uid = req.params.uid;
     var sql = "select * from requests where UID = ?;";
-    db.query(sql, [uid], function(err, result){
+    db.query(sql, [uid], function (err, result) {
         if (err) throw err;
         if (result.length > 0) res.send(result);
         else res.send("No request by you found!");
     });
 });
 
-app.get('/users/:uid/requests/:rid', function(req, res){
+app.get('/users/:uid/requests/:rid', function (req, res) {
     var uid = req.params.uid;
     var rid = req.params.rid;
     var sql = "select * from requests where UID = ? and ID = ?;";
-    db.query(sql, [uid,rid], function(err, result){
+    db.query(sql, [uid, rid], function (err, result) {
         if (err) throw err;
         if (result.length > 0) res.send(result);
         else res.send("No request by you with given Request ID!");
     });
 });
 
-app.post('/users',function(req, res){
+app.post('/users', function (req, res) {
     var name = req.body.name;
     var sql = "INSERT into users (Name) values (?);";
-    db.query(sql, [name], function(err, result){
+    db.query(sql, [name], function (err, result) {
         if (err) throw err;
         res.send("User added Successfully!");
     });
 });
 
-app.post('/users/:uid/visit', function(req, res){
+app.post('/users/:uid/visit', function (req, res) {
     var uid = req.params.uid;
     var description = req.body.description;
     var destination = req.body.destination;
     var timestamp = req.body.timestamp;
     var sql = "INSERT into visits (Description, Destination, Timestamp, UID) values (?, ?, ?, ?);";
-    db.query(sql, [description, destination, timestamp, uid], function(err, result){
+    db.query(sql, [description, destination, timestamp, uid], function (err, result) {
         if (err) throw err;
         res.send("Your visit is registered!");
     });
 });
 
-app.post('/visits/:vid/request', function(req, res){
+app.post('/visits/:vid/request', function (req, res) {
     var vid = req.params.vid;
     var description = req.body.description;
     var uid = req.body.uid;
     var sql = "INSERT into requests (Description, UID, VID) values (?, ?, ?);";
-    db.query(sql, [description, vid, uid], function(err, result){
+    db.query(sql, [description, vid, uid], function (err, result) {
         if (err) throw err;
-        res.send("Your Request to visit having ID "+ vid + " submitted successfully!");
+        res.send("Your Request to visit having ID " + vid + " submitted successfully!");
     });
 });
 
-app.put('/users/:uid/visits/:vid/requests/:rid', function(req, res){
+app.put('/users/:uid/visits/:vid/requests/:rid', function (req, res) {
     var rid = req.params.rid;
     var status = req.body.status;
     var sql = "UPDATE requests SET status = ? WHERE ID = ?;";
-    db.query(sql, [status, rid], function(err, result){
+    db.query(sql, [status, rid], function (err, result) {
         if (err) throw err;
         res.send("Request Status Updated!");
     });
 });
 
-app.put('/users/:uid/visits/:vid', function(req, res){
+app.put('/users/:uid/visits/:vid', function (req, res) {
     var uid = req.params.uid;
     var vid = req.params.vid;
     var status = req.body.status;
-    var sql = "SELECT count(*) as notavailable from requests WHERE status = 'Not Available' and VID = ?;";
-    db.query(sql, [vid], function(err, result){
+    var sql = "SELECT count(*) as notavailable from requests WHERE (status = 'Not Available' OR status = 'Accepted' OR status = 'Requested') and VID = ?;";
+    db.query(sql, [vid], function (err, result) {
         if (err) throw err;
         var na_requests = result[0]["notavailable"];
-        if (na_requests > 0){
+        if (na_requests > 0) {
             res.send("Sorry! You cannot complete a visit. There are " + na_requests + " requests pending on your visit.")
         }
         else {
-            var sql = "SELECT count(*) as completed from requests WHERE status = 'Completed' and VID = ?;";
-            db.query(sql, [vid], function(err, result){
+            var sql = "SELECT points from users WHERE ID = ?;";
+            db.query(sql, [uid], function (err, result) {
                 if (err) throw err;
-                var completed_requests = result[0]["completed"];
-                console.log(completed_requests);
-                if (completed_requests > 0){
-                    var points = (completed_requests * 5) + 5; 
-                    var sql = "UPDATE users SET points = ? WHERE ID = ?;";
-                    db.query(sql, [points, uid], function(err, result){
-                        if (err) throw err;
-                        res.send("Visit Status Updated!")
-                    })
-                }
-                var sql = "UPDATE visits SET status = ? WHERE ID = ?;";
-                db.query(sql, [status, vid], function(err, result){
+                var previous_points = result[0]["points"];
+                var sql = "SELECT count(*) as completed from requests WHERE status = 'Completed' and VID = ?;";
+                db.query(sql, [vid], function (err, result) {
                     if (err) throw err;
-                    res.send("Visit Status Updated!");
+                    var completed_requests = result[0]["completed"];
+                    if (completed_requests > 0) {
+                        var points = (completed_requests * 5) + 5;
+                        points = previous_points + points;
+                        var sql = "UPDATE users SET points = ? WHERE ID = ?;";
+                        db.query(sql, [points, uid], function (err, result) {
+                            if (err) throw err;
+                            var sql = "UPDATE visits SET status = ? WHERE ID = ?;";
+                            db.query(sql, [status, vid], function (err, result) {
+                                if (err) throw err;
+                                res.send("Visit Status Updated!");
+                            });
+                        });
+                    }
                 });
-            });        
+            });
         }
     });
 });
 
-app.put('/users/:uid/requests/:rid', function(req, res){
+app.put('/users/:uid/requests/:rid', function (req, res) {
     var uid = req.params.uid;
     var rid = req.params.rid;
     var status = req.body.status;
     var sql = "UPDATE requests SET status = ? WHERE ID = ?;";
-    db.query(sql, [status, rid], function(err, result){
+    db.query(sql, [status, rid], function (err, result) {
         if (err) throw err;
         res.send("Request Status Updated!");
     });
